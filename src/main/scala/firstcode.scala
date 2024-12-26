c6d46cfdb10050615405e6158d9d24e73d8db35f
Anilkumar9636 <anilkumarreddy9636@gmail.com>
2024-12-26 19:31:52 +0530

    Commiting first program

--- src/main/scala/firstcode.scala
+++ src/main/scala/firstcode.scala
@@ -3,8 +3,9 @@ object firstcode {
   def main(args:Array[String]):Unit=
     {
       println("Hello World")
-      val list=List(1,2,3,4,5)
+      val list=List(1,2,3,4,5,6,1,4,5)
       list.foreach(println)
+      list.foreach(print)
     }
 
 }
